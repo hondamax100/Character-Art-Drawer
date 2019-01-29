81a7e94d558ad96c07372850b6ddf2c5b7045ca3
hondamax100 <maximitt@hotmail.com>
2019-01-30 05:48:05 +1100

    Updated the main.c file.
    Added comments for recommendations, added a feature to clear the workspace, added a main menu and changed the default resolution of the workspace to fit a 4K monitor terminal at 91x33 terminal resolution.

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <curses.h>
+
+//recommended resolution: 91x33
 
 int main(void){
 
-  int i; int j; int k; int l;                                                   //
-  char * rows[30][30];                                                          //
-  for(i=0;i<30;i++){                                                            //
-    for(j=0;j<30;j++){                                                          //
-      rows[i][j]=" - ";}}                                                       //
+  int i; int j; int k; int l;
+  char * rows[34][45];
+  for(i=0;i<34;i++){
+    for(j=0;j<45;j++){
+      rows[i][j]=" -";}}
 
-  //draw rectangle
   while (0==0){
 
     printf("\n");
-    for(k=0;k<30;k++){
-      for(l=0;l<30;l++){
+    for(k=0;k<34;k++){
+      for(l=0;l<45;l++){
         printf("%s", rows[k][l]);
       }
       printf("\n");
@@ -26,30 +26,29 @@ int main(void){
     printf("What do do? ");
     scanf("%d", &option);
     if (option==1){
-
       int len; int wid;
       int n=0; int m=1;
       int x; int y;
-
       printf("Coordinates: [x,y]");
       scanf("%d%d", &x, &y);
-
       printf("Size: [len/wid]");
       scanf("%d%d", &len, &wid);
-
       y=y-1;
       while (n<len) {
-        while (m<(wid+1)  ) {
-          rows[x+n][y+m]=" # ";
+        while (m<(wid+1)) {
+          rows[x+n][y+m]=" #";
           m++;
         }
         n++;
         m=1;
       }
     }
+    else if(option==0){
+      int i; int j; int k; int l;
+      for(i=0;i<34;i++){
+        for(j=0;j<45;j++){
+          rows[i][j]=" -";}}
+    }
   }
-
-
-
   return 0;
-};
+}
